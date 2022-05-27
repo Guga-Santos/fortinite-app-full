6971818964dfdef06d764ad04e3c0c8e84ef17d3
GugaSantos <guga.santos.guga@gmail.com>
2022-05-27 19:08:06 -0300

    Initial commit

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,16 @@
 import React from "react";
+import './Loading.css';
 
 export default function Loading() {
     return(
-        <h1> TO CARREGANDO...</h1>
+        <div id="load">
+            <div>G</div>
+            <div>N</div>
+            <div>I</div>
+            <div>D</div>
+            <div>A</div>
+            <div>O</div>
+            <div>L</div>
+        </div>
     )
 }
--- src/pages/Initial.js
+++ src/pages/Initial.js
@@ -2,6 +2,9 @@ import React from "react";
 import Header from "../components/Header";
 import NewsCard from '../components/NewsCard';
 import character3 from '../images/character3.png';
+import github from '../images/github.png';
+import instagram from '../images/instagram.png';
+import linkedin from '../images/linkedin.png';
 import "./Initial.css";
 
 export default function Initial() {
@@ -14,6 +17,35 @@ export default function Initial() {
                 <NewsCard />
             </div> 
             <div className="char-background-border" />
+                <div className="social-container">
+                <a  className="anchor-footer"
+                    href= "https://www.instagram.com/guga_marahu/"
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    >
+                    <img src={instagram} alt=" instagram icon" />
+                </a>
+                <a  className="anchor-footer"
+                    href= "https://www.linkedin.com/in/gugasantos/"
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    >
+                    <img src={linkedin} alt=" linkedin icon" />
+                    </a>
+                    <a  className="anchor-footer"
+                    href= "https://github.com/Guga-Santos"
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    >
+                    <img src={github} alt=" github icon" />
+                    </a>
+                </div>
+                <div className="copyright-infos">
+                    <h5> This app is NOT official and NOT endorsed by Epic Games in any way.
+                    All names, resources and generally any Fortnite related content belongs to Epic Games.</h5>   
+                    <h5>It's just a personal study of API requests and a solution to compare mine and my friends game statistics.</h5>
+                </div>
+
         </div>
     )
 }
