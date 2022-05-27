6f1fac7c473fe89e53a500389dae392fcb575016
GugaSantos <guga.santos.guga@gmail.com>
2022-05-27 16:26:33 -0300

    fix: BrowserRouter w/ no bsaename

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <FortniteProvider>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <App />
       </Router>
     </FortniteProvider>
